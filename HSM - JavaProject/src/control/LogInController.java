1c36dbc470c3623c650fcb11426d2f05b5059470
Diego Revenga <drevenga@ucm.es>
2022-03-11 00:14:55 +0100

    Activado el formateo al guardar

--- HSM - JavaProject/src/control/LogInController.java
+++ HSM - JavaProject/src/control/LogInController.java
@@ -8,32 +8,32 @@ import userManager.LoginAuthenticator;
 import userManager.LoginData;
 
 public class LogInController {
- 
+
 	public LogInController() {}
 
 	protected void doPost(String dni, String pass) throws SQLException, LogInException{
-	    
-	    LoginData loginData = new LoginData(dni, pass); 
-	    LoginAuthenticator loginAuthenticator = new LoginAuthenticator();
-	    
-	    try {
-	        UserRole userValidate = loginAuthenticator.authenticateUser(loginData);
-	        
-	        switch(userValidate) {
-	        case PATIENT: 
-	        	System.out.println("Patient's Home"); break;
-	        case DOCTOR:
-	        	System.out.println("Doctor's Home"); break;
-	        case ADMIN:
-	        	System.out.println("Admin's Home"); break;
-	        }
-	    }
-	    
-	    catch (SQLException sql_e) {
-	        throw sql_e;
-	    }
-	    catch (LogInException li_e) {
-	        throw li_e;
-	    } 
+
+		LoginData loginData = new LoginData(dni, pass);
+		LoginAuthenticator loginAuthenticator = new LoginAuthenticator();
+
+		try {
+			UserRole userValidate = loginAuthenticator.authenticateUser(loginData);
+
+			switch(userValidate) {
+			case PATIENT:
+				System.out.println("Patient's Home"); break;
+			case DOCTOR:
+				System.out.println("Doctor's Home"); break;
+			case ADMIN:
+				System.out.println("Admin's Home"); break;
+			}
+		}
+
+		catch (SQLException sql_e) {
+			throw sql_e;
+		}
+		catch (LogInException li_e) {
+			throw li_e;
+		}
 	}
 }
