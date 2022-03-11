ea08558af198b43768e12d6566723b637eba879d
mohaa002 <atmohamed802@gmail.com>
2022-03-11 12:15:54 +0100

    Prueba

--- HSM - JavaProject/src/launcher/main.java
+++ HSM - JavaProject/src/launcher/main.java
@@ -2,7 +2,7 @@ package launcher;
 
 public class main {
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
+		System.out.println("Hola mundo ");
+		System.out.println("prueba ");
 	}
-
 }
