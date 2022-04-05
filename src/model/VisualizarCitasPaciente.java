6819ea22094d02408d946b3676a511785ee60957
mohaa002 <atmohamed802@gmail.com>
2022-04-05 12:44:28 +0200

    VisualizarCitasPendientes

--- src/model/VisualizarCitasPaciente.java
+++ src/model/VisualizarCitasPaciente.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.sql.Array;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
@@ -13,11 +14,21 @@ public class VisualizarCitasPaciente {
 
 	private void visualizar() throws SQLException {
 		Connection con = DBConnector.connectdb();
-		String SQL = "SELECT date, hour, name, lastname FROM appointments join users WHERE dni = dni";
+		String SQL = "SELECT date, hour, name || ' ' || lastname AS doctor FROM appointments join users WHERE dni = dni order by date";
 
 		Statement st = con.createStatement();
 
 		ResultSet resultSet = st.executeQuery(SQL);
+
+		if (!resultSet.next()) {
+			// Lista vacia, informamos al usuario de ello
+		}
+		else {
+			Array c1 = resultSet.getArray("date");
+			Array c2 = resultSet.getArray("hour");
+			Array c3 = resultSet.getArray("doctor");
+
+		}
 
 
 
